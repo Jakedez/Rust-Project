964807dd21630caead986b91824a8e75f0fc9d26
Jakedez <99777297+Jakedez@users.noreply.github.com>
2023-02-22 11:15:13 -0700

    Completed Project

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-use std::io::{stdin,stdout,Write};
+use std::{io::{stdin,stdout,Write}};
+
 
 
 struct Person{
     name: String,
-    age: i32
+    age: i32,
+    career: String
 }
 
-fn get_char(dialog : String) -> char{
-    let character : char;
-    print!("{dialog}");
-    stdout().flush().unwrap();
-    let _ = stdout().flush();
-    let mut input_line: String = String::new();
-    return 'c';
-}
+
+
+
+
 
 fn get_int(dialog : String) -> i32{
     print!("{dialog}");
@@ -43,16 +41,72 @@ fn get_string(dialog : String) -> String{
 }
 
 fn get_person() -> Person{
-    let person: Person = Person { name: get_string("Please type a name: ".to_string()), age: get_int("Please type an age: ".to_string()) };
+    let person: Person = Person { name: get_string("Please type a name: ".to_string()), age: get_int("Please type an age: ".to_string()), career: get_string("Please enter your career choice: ".to_string())};
     return person; 
 }
 
+fn read_age(person : Person) -> Person{
+    println!("\n\n\nAge: {}\n\n\n", person.age);
+    return person;
+}
+
+fn print_menu(){
+    println!("A. Change Career");
+    println!("B. Change all account info");
+    println!("C. Get Current Age");
+    println!("D. Display all info");
+    println!("E. Exit");
+    return;
+}
+
+fn display_all(account : Person) -> Person{
+    println!("\n\n\nName: {}", account.name);
+    println!("Age: {}", account.age);
+    println!("Career: {}\n", account.career);
+    println!("");
+    return account;
+}
+
 fn main() {
-    let hello: Person = get_person();
-    println!("Name: {}, Age: {}", hello.name, hello.age);
+    let mut account : Person = get_person();
+    println!("\n\n\n");
+
     let mut running : bool = true;
+
+    let mut response : String;
+
     while running {
-        running = false;
+        print_menu();
+        response = get_string("Please select an option: ".to_string()).to_lowercase();
+
+        if response == "e".to_string(){
+            running = false;
+            println!("\n\n\n");
+        }
+        else if response == "d".to_string(){
+            account = display_all(account);
+        }
+        else if response == "c".to_string(){
+            account = read_age(account);
+        }
+        else if response == "b".to_string() {
+            println!("\n\n\n");
+            account = get_person();
+            println!("\n\n\n");
+        }
+        else if response == "a".to_string(){
+            account.career = get_string("\n\n\nPlease type a new career choice: ".to_string());
+            println!("\n\n\n");
+        }
+        else{
+
+        }
+        
+
+
     }
+
+    println!("Goodbye {}!", account.name);
+    
     return;
 }
